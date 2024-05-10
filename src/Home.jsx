57a04957f5f2c3320649afcd2b7ab2645dec8d73
Mahbub Maruf <mahbuburrahman8570@gmail.com>
2024-05-10 23:12:20 +0600

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import LogoOne from "./images/logoOne.png";
 import LogoTwo from "./images/hero-image 2.jpg";
 import LogoThree from "./images/Big Dream.jpg";
+import LogoFour from "./images/about 3.png";
+import LogoFive from "./images/king 4.png";
 
 export default function Home() {
   return (
@@ -13,13 +15,40 @@ export default function Home() {
       </header>
       <section
         id="SectionOne"
-        className="bg-gradient-to-r from-[#F8F7B6] to-[#BCD5FC] h-auto"
+        className="bg-gradient-to-r from-[#F8F7B6] to-[#BCD5FC]"
       >
         <div id="ImgOne">
           <img src={LogoTwo} className="rounded-2xl" />
         </div>
-        <div id="ImgTwo" className="my-20">
+        <div id="ImgTwo" className="my-10">
           <img src={LogoThree} />
+        </div>
+      </section>
+
+      {/* Section Two */}
+
+      <section
+        id="SectionTwo"
+        className="flex flex-col justify-center items-center border-2 border-yellow-400 m-5"
+      >
+        <div id="LogoFour">
+          <img src={LogoFour} />
+        </div>
+        <div className="p-5">
+          <h1 className="text-center">Small Coin, Big Dreams!</h1>
+          <p className="text-center">
+            MiiniSol is designed to be the spirited mini version of Solana,
+            capturing the essence and excitement of the blockchain in a more
+            compact, community-focused form. We aim to create a vibrant
+            ecosystem that supports the growth and hype of memecoins, empowering
+            both novice and seasoned investors to explore innovative and
+            entertaining opportunities in the cryptocurrency space. We will
+            mostly inspire investors to keep the hype and support memecoins by
+            launching New Memecoin Projects on our multi-chain IDOs.
+          </p>
+        </div>
+        <div id="LogoFive">
+          <img src={LogoFive} />
         </div>
       </section>
     </div>
